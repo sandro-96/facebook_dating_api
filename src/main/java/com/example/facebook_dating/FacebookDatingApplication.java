71de6b0193d73ad9c686f70258574ee0221e6415
sandro-96 <trichuthanh@gmail.com>
2023-12-26 13:25:47 +0700

    first commit

---
+++ src/main/java/com/example/facebook_dating/FacebookDatingApplication.java
@@ -0,0 +1,13 @@
+package com.example.facebook_dating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebookDatingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacebookDatingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/facebook_dating/FacebookDatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.facebook_dating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacebookDatingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
