b7180a10a6b2573dc716c44ad56c24def26fd6bc
sandro-96 <trichuthanh@gmail.com>
2024-01-16 15:24:22 +0700

    update user controller

--- src/main/java/com/fbd/controller/UserController.java
+++ src/main/java/com/fbd/controller/UserController.java
@@ -1,7 +1,8 @@
 package com.fbd.controller;
 
+import com.fbd.dto.LoginDTO;
 import com.fbd.model.User;
-import com.fbd.mongo.UserRepository;
+import com.fbd.service.UserService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -10,14 +11,21 @@ import org.springframework.web.bind.annotation.*;
 @Log4j2
 @RequestMapping("/user")
 public class UserController {
-
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
+    @PostMapping("/login")
+    public User login(@RequestBody LoginDTO loginDTO) {
+        return userService.login(loginDTO);
+    }
 
-    @PostMapping("/create")
-    public User create(@RequestBody User user) {
-        log.info(user);
-        return userRepository.insert(user);
+    @PostMapping("/signup")
+    public User signup(@RequestBody User user) {
+        return userService.signup(user);
+    }
+
+    @GetMapping("/checkUserName")
+    public Boolean checkUserName(@RequestParam String userName) {
+        return userService.checkUserName(userName);
     }
 }
