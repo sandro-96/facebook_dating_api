2ba9864bf2c5688ebf49b684dae69921ac1688ca
sandro <sandro.vnib@gmail.com>
2024-05-13 16:09:03 +0700

    update somethings

--- src/main/java/com/fbd/model/ChatMessage.java
+++ src/main/java/com/fbd/model/ChatMessage.java
@@ -1,22 +1,27 @@
 package com.fbd.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
-
-@Setter
-@Getter
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Document(collection = "chat")
 public class ChatMessage {
+    @Id
+    private String id;
     private String forUserId;
     private String topicId;
     private String content;
     private String imagePath;
+    @Builder.Default
+    private int emoji = 0;
     @CreatedDate
     @Indexed
     private Date createdAt;
